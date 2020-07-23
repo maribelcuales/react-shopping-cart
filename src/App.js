8e1669c30fb6d0b461417f64d484309d18d1b98b
maribelcuales <maribelcuales@gmail.com>
2020-07-24 04:57:05 +0800

    Wrapped components with CartContext Provider in App.js.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 import { ProductContext } from './contexts/ProductContext'; 
+import { CartContext } from './contexts/CartContext'; 
 
 function App() {
 	const [products] = useState(data);
@@ -23,17 +24,19 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products /> 
-					{/* <Products products={products} addItem={addItem} /> */}
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products /> 
+						{/* <Products products={products} addItem={addItem} /> */}
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
