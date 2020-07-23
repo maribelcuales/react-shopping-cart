65c5883080ebdfc62e3e664576efbac6e10d91ed
maribelcuales <maribelcuales@gmail.com>
2020-07-24 04:46:02 +0800

    Import useContext and ProductContext then called the useContext hook.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-import { ProductContext } from './contexts'; 
+import { ProductContext } from './contexts/ProductContext'; 
 
 function App() {
 	const [products] = useState(data);
