b2eb78990281d71fce51f8427a7df5ce84c582f7
maribelcuales <maribelcuales@gmail.com>
2020-07-24 05:07:05 +0800

    Called useContext hook and passed in CartContext.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { CartContext } from '../contexts/CartContext';
+
 const Navigation = props => {
+	const cart = useContext(CartContext); 
+	console.log({ cart });
+	
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
